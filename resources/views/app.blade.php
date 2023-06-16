86db93283f3684af3601dd3f8d4e61da62de003c
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2023-06-16 09:58:12 +0600

    inertia setup: server side

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    @vite('resources/js/app.js')
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
+
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +144,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
