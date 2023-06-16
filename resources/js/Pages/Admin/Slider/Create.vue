4b2e9ced2064e0d44587379c7164139aec1ad87b
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2023-06-16 11:57:29 +0600

    setup slider

---
+++ resources/js/Pages/Admin/Slider/Create.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="main-panel">
+    <div class="content-wrapper">
+      <div class="page-header">
+        <h3 class="page-title">
+          <span class="page-title-icon bg-gradient-primary text-white me-2">
+            <i class="mdi mdi-home"></i>
+          </span>
+          Dashboard
+        </h3>
+        <nav aria-label="breadcrumb">
+          <ul class="breadcrumb">
+            <li class="breadcrumb-item active" aria-current="page">
+              <span></span>Slider/Create
+              <i
+                class="
+                  mdi mdi-alert-circle-outline
+                  icon-sm
+                  text-primary
+                  align-middle
+                "
+              ></i>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12 grid-margin stretch-card">
+          <div class="card">
+            <div class="card-body">
+              <!-- <h4 class="card-title">Default form</h4>
+              <p class="card-description">Basic form layout</p> -->
+              <form class="forms-sample">
+                <div class="form-group">
+                  <label for="sl-position">Position</label>
+                  <input
+                    type="integer"
+                    class="form-control"
+                    id="sl-position"
+                    placeholder="Slider Position"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="sl-img">Choose Image</label>
+                  <input
+                    type="file"
+                    class="form-control"
+                    id="sl-img"
+                  />
+                </div>
+                <!-- <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Password"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputConfirmPassword1"
+                    >Confirm Password</label
+                  >
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="exampleInputConfirmPassword1"
+                    placeholder="Password"
+                  />
+                </div>
+                <div class="form-check form-check-flat form-check-primary">
+                  <label class="form-check-label">
+                    <input type="checkbox" class="form-check-input" /> Remember
+                    me <i class="input-helper"></i
+                  ></label>
+                </div> -->
+                <button type="submit" class="btn btn-gradient-primary me-2">
+                  Submit
+                </button>
+                <a href="/slider" class="btn btn-light">Back</a>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminLayout from "../../../Shared/AdminLayout";
+export default {
+    
+  layout: AdminLayout,
+};
+</script>
+
+<style>
+</style>
---
+++ resources/js/Pages/Admin/Slider/Index.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <div class="page-header">
+                <h3 class="page-title">
+                    <span class="page-title-icon bg-gradient-primary text-white me-2">
+                        <i class="mdi mdi-home"></i>
+                    </span> Dashboard
+                </h3>
+                <nav aria-label="breadcrumb">
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item active" aria-current="page">
+                            <span></span>Overview <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <inertia-link class="btn btn-primary btn-sm card-title mb-4 text-right" href="/slider/create">Create</inertia-link>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th> # </th>
+                                <th> First name </th>
+                                <th> Progress </th>
+                                <th> Amount </th>
+                                <th> Deadline </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> 1 </td>
+                                <td> Herman Beck </td>
+                                <td>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </td>
+                                <td> $ 77.99 </td>
+                                <td> May 15, 2015 </td>
+                            </tr>
+                            <tr>
+                                <td> 2 </td>
+                                <td> Messsy Adam </td>
+                                <td>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </td>
+                                <td> $245.30 </td>
+                                <td> July 1, 2015 </td>
+                            </tr>
+                            <tr>
+                                <td> 3 </td>
+                                <td> John Richards </td>
+                                <td>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </td>
+                                <td> $138.00 </td>
+                                <td> Apr 12, 2015 </td>
+                            </tr>
+                            <tr>
+                                <td> 4 </td>
+                                <td> Peter Meggik </td>
+                                <td>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </td>
+                                <td> $ 77.99 </td>
+                                <td> May 15, 2015 </td>
+                            </tr>
+                            <tr>
+                                <td> 5 </td>
+                                <td> Edward </td>
+                                <td>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </td>
+                                <td> $ 160.25 </td>
+                                <td> May 03, 2015 </td>
+                            </tr>
+                            <tr>
+                                <td> 6 </td>
+                                <td> John Doe </td>
+                                <td>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </td>
+                                <td> $ 123.21 </td>
+                                <td> April 05, 2015 </td>
+                            </tr>
+                            <tr>
+                                <td> 7 </td>
+                                <td> Henry Tom </td>
+                                <td>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </td>
+                                <td> $ 150.00 </td>
+                                <td> June 16, 2015 </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import AdminLayout from "../../../Shared/AdminLayout";
+export default {
+
+    layout: AdminLayout,
+};
+</script>
+
+<style>
+</style>
