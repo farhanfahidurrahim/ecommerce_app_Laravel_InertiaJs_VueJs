b4579df1dddab3f6d72128dcc4df89e713e3c662
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2023-06-17 10:59:46 +0600

    slider update

--- resources/js/Pages/Admin/Slider/Index.vue
+++ resources/js/Pages/Admin/Slider/Index.vue
@@ -5,7 +5,7 @@
                 <h3 class="page-title">
                     <span class="page-title-icon bg-gradient-primary text-white me-2">
                         <i class="mdi mdi-home"></i>
-                    </span> Dashboard
+                    </span> Sliders
                 </h3>
                 <nav aria-label="breadcrumb">
                     <ul class="breadcrumb">
