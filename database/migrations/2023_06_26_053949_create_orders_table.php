fb9336fa9eb0f05bab259956b8deed6634303414
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2023-06-26 11:50:47 +0600

    create order table

---
+++ database/migrations/2023_06_26_053949_create_orders_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->enum('status',['pending','processing','completed','cancel']);
+            $table->decimal('grand_total',20,6);
+            $table->unsignedInteger('item_count');
+
+            $table->string('payment_method');
+            $table->boolean('payment_status')->default(0);
+
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->text('address');
+            $table->string('country');
+            $table->string('city');
+            $table->string('district');
+            $table->string('postal_code');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
